155f7007a4d34fbb7b0b9f78f574dc190ecc4b53
hakmoe <haakon.oyehaug.moe@gmail.com>
2025-07-21 15:43:36 +0200

    removed error

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -9,46 +9,61 @@ return {
   -- Game
   { "ThePrimeagen/vim-be-good" },
 
-  -- LSP installer
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-
-  -- Mason + LSP integration
+  -- LSP + Mason + LSPConfig all together (modern setup)
   {
     "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig",
+      "hrsh7th/cmp-nvim-lsp",
+    },
     config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pyright" },
-        automatic_installation = true,
-      })
-    end,
-  },
+      require("mason").setup()
 
-  -- Core LSP setup
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
       local lspconfig = require("lspconfig")
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      require("mason-lspconfig").setup_handlers({
-        function(server_name)
-          lspconfig[server_name].setup({
-            capabilities = capabilities,
-            on_attach = function(_, bufnr)
-              local opts = { buffer = bufnr }
-              vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-              vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-              vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-              vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-            end,
-          })
-        end,
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "pyright" },
+        automatic_installation = true,
+
+        -- 🔄 Modern handler approach (replaces setup_handlers)
+        handlers = {
+          -- Default handler for all servers
+          function(server_name)
+            lspconfig[server_name].setup({
+              capabilities = capabilities,
+              on_attach = function(_, bufnr)
+                local opts = { buffer = bufnr }
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+              end,
+            })
+          end,
+
+          -- Optional: Custom config for lua_ls
+          ["lua_ls"] = function()
+            lspconfig.lua_ls.setup({
+              capabilities = capabilities,
+              settings = {
+                Lua = {
+                  diagnostics = {
+                    globals = { "vim" },
+                  },
+                },
+              },
+              on_attach = function(_, bufnr)
+                local opts = { buffer = bufnr }
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+              end,
+            })
+          end,
+        },
       })
     end,
   },
